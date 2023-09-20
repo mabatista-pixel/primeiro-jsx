5919f0623a765db4469b863097ab7f7e630d4aef
Matheus Batista <mabatista_10@hotmail.com>
2023-09-19 21:58:14 -0300

    feat: Primeiros passos com React e JSX

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+
+  const firstObj = {
+    name: "Matheus",
+    module: "M3",
+    age: 29
+  }
+
+  return (
+    <>
+      <div>
+        <li>Olá, meu nome é {firstObj.name}, estudante do módulo {firstObj.module} e atualmente possuo {firstObj.age} anos de idade.</li>
+      </div>
+      <button onClick={() => alert(`Olá, ${firstObj.name}`) }>Clique aqui</button>
+    </>
+  )
+}
+
+export default App
